c6dad28c7835a2add73585a953218247bc9c8042
alcss <fernandoalcalacasas@hotmail.com>
2022-09-24 22:27:07 -0500

    Commit antes de intentar otra cosa para rellenar la matriz

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 struct Rectangulo {
     alto: i32,
     ancho: i32
@@ -18,13 +17,13 @@ fn main() {
             let amount_bins_ancho:i32= w_a.ancho / bins.ancho;
             let amount_bins_alto: i32 = w_a.alto / bins.alto;
 
-            println!("Bins ancho: {}", amount_bins_ancho);
-            println!("Bins alto: {}", amount_bins_alto);
+            println!("Caben  {} Bins a lo ancho", amount_bins_ancho);
+            println!("Caben  {} Bins a lo alto", amount_bins_alto);
 
             //ESTA ES LA CANTIDAD DE BINS QUE CABEN EN EL WORK AREA
             //let amount_bins = cmp::min(amount_bins_alto, amount_bins_ancho);
             let amount_bins = amount_bins_ancho*amount_bins_alto;
-
+            
 
             //LLENAMOS EL ARREGLO DEL WORK AREA CON EL NUMERO DEL BIN PARA VIZUALIZARLOS
             let mut j_w_a: i32;
@@ -33,15 +32,20 @@ fn main() {
             for i in 0..amount_bins {
 
                 for j in 0..bins.alto {
-                    j_w_a = j+(i%bins.alto*bins.alto);
+                    //j_w_a = j+(i%bins.alto*bins.alto);
+                    j_w_a = j+((i%(bins.alto))*bins.alto);
+                    
 
                     for k in 0..bins.ancho{
-                        k_w_a = k+((i%bins.ancho*bins.ancho));
-                        wa_space_array[j_w_a as usize][k_w_a as usize] = i+1;
+                        //k_w_a = k+(i%bins.ancho*bins.ancho);
+                        k_w_a = k+((i%(bins.ancho)) * bins.ancho);
+                        println!("I: {} = j_w_a: {} | {}, k_w_a: {} | {}", i, j_w_a, j, k_w_a, k);
+                        //wa_space_array[j_w_a as usize][k_w_a as usize] = i+1;
+                        //  mostrar_array(&wa_space_array, &w_a);
                     }
                 }
             }
-            mostrar_array(wa_space_array, &w_a);
+            mostrar_array(&wa_space_array, &w_a);
             //mostrar Bins En el Work Area
 
 
@@ -81,7 +85,7 @@ fn inicializar_space_array(w_a: &Rectangulo)->Vec<Vec<i32>>{
     }
     w_a_space
 }
-fn mostrar_array(array: Vec<Vec<i32>>, w_a: &Rectangulo){
+fn mostrar_array(array: &Vec<Vec<i32>>, w_a: &Rectangulo){
     for (i, _col) in (0..w_a.alto).enumerate() {
         for (j, _raw) in (0..w_a.ancho).enumerate(){
             print!("[{}]", array[i][j]);

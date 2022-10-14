71d93f911782e1e5202887ec65fb4593b6798d64
alcss <fernandoalcalacasas@hotmail.com>
2022-10-13 22:55:03 -0500

    Terminada ecuación para obtener el pertenecientes a un item, falta obtener a hora los i, y j

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,10 @@ fn main() {
             let mut wa_space_array: Vec<Vec<char>> = inicializar_space_array(&w_a);
                                  
             //CALCULAR CANTIDAD DE BINS EN EL WORK_AREA
-                //Bins = TamañoWA/TamañoBins
             let amount_bins_ancho:i32= w_a.ancho / bins.ancho;
             let amount_bins_alto: i32 = w_a.alto / bins.alto;
+            let cant_bins = amount_bins_alto*amount_bins_ancho;
+            let mut bins_array: Vec<Vec<Vec<char>>>= Vec::new();
 
             println!("Caben  {} Bins a lo ancho", amount_bins_ancho);
             println!("Caben  {} Bins a lo alto", amount_bins_alto);
@@ -41,7 +42,7 @@ fn main() {
             imprimir_items(&items);
             
             //COMENZAR A ALMACENAR LOS ITEMS EN LOS CONTENEDORES
-
+            colocar_items(&items, &bins, cant_bins, bins_array)
             
         }
         else {
@@ -53,12 +54,64 @@ fn main() {
         
     }    
 }
-fn colocar_items(items: &Vec<Rec>, wa_space_array: &mut Vec<Vec<char>>, bins: Rec){
-    let mut indice: (usize, usize) = (0 , 0);
-    let mut tam_cont:(i32, i32) = (bins.alto, bins.ancho);
-    let mut disponible: Vec<(i32, i32)> = Vec::new();
-    for i in 0..items.len() { //RECORREMOS LOS ITEMS
-        disponible.push((tam_cont.0 - indice.0 as i32,  tam_cont.1 - indice.1 as i32));
+fn colocar_items(items: &Vec<Rec>, bins: &Rec, cant_bins: i32, mut bins_array: Vec<Vec<Vec<char>>>){
+    //INICIALIZAMOS LOS BINS CON 0s
+    for _i in 0..cant_bins{
+        bins_array.push(inicializar_space_array(&bins))
+    }
+    //RECORREMOS LOS ITEMS
+    for i in 0..items.len() { 
+        acomodar(bins.clone(), cant_bins, &bins_array, items, i);
+    }
+}
+fn acomodar(bins: Rec, cant_bins: i32, bins_array: &Vec<Vec<Vec<char>>>, items: &Vec<Rec>, indice: usize){
+    for b in 0..cant_bins as usize{//RECORREMOS LOS CONTENEDORES
+        let mut disp : Vec<(usize, usize)> = Vec::new(); //ARREGLO PARA ALMACENAR LOS INDICES DISPONIBLES
+        let mut contador:i32=0; //CONTADOR PARA SABER EL AREA DISPONIBLE
+        for i in 0..bins.alto as usize{ //FILAS
+            for j in 0..bins.ancho as usize{ //COLUMNAS
+                //CONTAMOS ESPACIOS DISPONIBLES
+                if  bins_array[b][i][j] == char::from_u32(48 as u32).unwrap(){ //COMPARAMOS QUE EL ARRAY CONTENGA UN 0 (0= DISPONIBLE)
+                    contador+=1; //ACTUALIZAMOS EL CONTADOR
+                    disp.push((i, j)); //GUARDAMOS LOS SUBINDICES DEL ESPACIO
+                }
+            }
+        }
+        
+        if contador < items[indice].area{
+            //IF EL AREA DISPONIBLE ES MENOR QUE EL AREA DEL ITEM ACTUAL PASAMOS AL SIGUIENTE CONTENEDOR
+            continue;
+        }
+        //REVISAR QUE LOS ESPACIOS VACIÓS SEAN USABLES POR EL ITEM
+
+        //RECORREMOS LOS ESPACIOS DISPONBIES, MENOS LOS ÚLTIMOS (AREA DEL RECTANGULO) PORQUE COMPARAMOS CADA I CON SUS (AREA DEL RECTANGULO) SIGUIENTES
+
+        for i in 0..(disp.len()-(items[indice].ancho*items[indice].alto) as usize){
+            //CADA ITERACIÓN ES UN POSIBLE LUGAR DONDE PONER EL ITEM
+            println!("\n");
+            //COMPARAMOS CADA I CON (SUS AREA DEL RECTANGULO) SIGUIENTES
+            for j in i..(items[indice].ancho*items[indice].alto) as usize{
+                //CADA ITERACIÓN ES UN INDICE SIGUIENTE DEL ITEM DESDE EL INDICE I
+
+                //ESTO NOS GENERA LOS INDICES QUE DEBERÍAN ESTAR DISPONIBLES PARA GUARDAR EL ITEM PARTIENDO DESDE I
+                //A PARTIR DE UN SOLO INDICE
+                if i + items[indice].ancho as usize <= bins.ancho as usize {
+                    let desde:f64 = i as f64;
+                    let an_i: f64 = items[indice].ancho as f64; 
+                    let len_f64:f64 = bins.ancho as f64;
+                    let jump_fl:f64 = (j as f64 / an_i as f64).floor();
+                    let j_f64:f64 = j as f64;
+
+                    let ec = ((desde + (jump_fl * len_f64)) + j_f64 - jump_fl * an_i) as usize;
+                    println!("ec: {}", ec);
+                    
+                }
+                //CON LOS DOS ÍNDICES
+                if bins_array[b][disp[j].0][disp[j].1] == char::from_u32(48 as u32).unwrap(){
+                    contador+=0;
+                }
+            }
+        }
     }
 }
 fn pedir_items(bins: &Rec)->Vec<Rec>{
@@ -172,3 +225,4 @@ fn imprimir_items(items: &Vec<Rec>){
         println!("H: {}, W: {}, A:{}", items[i].alto, items[i].ancho, items[i].area);
     }
 }
+
